2650474c33b122447423cf2a6688942322f9fea7
Nick Webster <nick@nick.geek.nz>
2014-08-04 17:22:05 +1200

    Cleaning up some old code

--- noughts.py
+++ noughts.py
@@ -5,12 +5,14 @@ import time
 
 server = raw_input("\n\nServer to connect to: ")
 channel = "#noughts.py"
-botnick = "noughts"
+botnick = "noughts_"
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((server, 6667))																												 #connects to the server
 irc.send("USER "+ botnick +" "+ botnick +" "+ botnick +" :Wanna play noughts and crosses?\n")
 irc.send("NICK "+ botnick +"\n")
+time.sleep(2)
+#irc.send("NS IDENTIFY "+raw_input("Password: "))
 irc.send("JOIN "+ channel +"\n")
 
 gameStats = {}
@@ -18,6 +20,13 @@ noughts = []
 crosses = []
 
 #Functions
+def drawTable():
+	sendPrivateMessage(username, "+-----------+")
+	for i in range(0, 9):
+		sendPrivateMessage(username, "| "+gameStats["table"][i+1]+" | "+gameStats["table"][i+1]+" | "+gameStats["table"][i+1]+" |")
+	sendPrivateMessage(username, "+-----------+")
+	sendPrivateMessage(username, "Key: o = nought, x = cross and / = neutral")
+
 def startGame():
 	start = random.randrange(2)
 	table = {"1":"/","2":"/","3":"/","4":"/","5":"/","6":"/","7":"/","8":"/","9":"/"}
@@ -34,12 +43,7 @@ def startGame():
 	#Announce the new game
 	send("A new game has started. "+startingTeam+" starts.")
 	send("Table:")
-	send("+-----------+")
-	send("| "+gameStats["table"]["1"]+" | "+gameStats["table"]["2"]+" | "+gameStats["table"]["3"]+" |")
-	send("| "+gameStats["table"]["4"]+" | "+gameStats["table"]["5"]+" | "+gameStats["table"]["6"]+" |")
-	send("| "+gameStats["table"]["7"]+" | "+gameStats["table"]["8"]+" | "+gameStats["table"]["9"]+" |")
-	send("+-----------+")
-	send("Key: o = nought, x = cross and / = neutral")
+	drawTable()
 	send("Each item on the grid is numbered from left to right starting at 1")
 
 	global noughts
@@ -49,22 +53,12 @@ def startGame():
 
 def game(username):
 	sendPrivateMessage(username, "Table:")
-	sendPrivateMessage(username, "+-----------+")
-	sendPrivateMessage(username, "| "+gameStats["table"]["1"]+" | "+gameStats["table"]["2"]+" | "+gameStats["table"]["3"]+" |")
-	sendPrivateMessage(username, "| "+gameStats["table"]["4"]+" | "+gameStats["table"]["5"]+" | "+gameStats["table"]["6"]+" |")
-	sendPrivateMessage(username, "| "+gameStats["table"]["7"]+" | "+gameStats["table"]["8"]+" | "+gameStats["table"]["9"]+" |")
-	sendPrivateMessage(username, "+-----------+")
-	sendPrivateMessage(username, "Key: o = nought, x = cross and / = neutral")
+	drawTable()
 	sendPrivateMessage(username, "Current move: "+gameStats["currentMove"])
 
 def announceGame():
 	send("Table:")
-	send("+-----------+")
-	send("| "+gameStats["table"]["1"]+" | "+gameStats["table"]["2"]+" | "+gameStats["table"]["3"]+" |")
-	send("| "+gameStats["table"]["4"]+" | "+gameStats["table"]["5"]+" | "+gameStats["table"]["6"]+" |")
-	send("| "+gameStats["table"]["7"]+" | "+gameStats["table"]["8"]+" | "+gameStats["table"]["9"]+" |")
-	send("+-----------+")
-	send("Key: o = nought, x = cross and / = neutral")
+	drawTable()
 	send("Current move: "+gameStats["currentMove"])
 
 def sendPrivateMessage(username, message):
@@ -133,17 +127,23 @@ def gameover():
 time.sleep(1)
 startGame()
 while 1:
+	time.sleep(0.1)
 	gameover()
-	output = irc.recv(2040)
+	output = irc.recv(4096)
+	if output.find ( 'PING' ) != -1:
+		irc.send("PONG "+output.split(" ")[1]+'\r\n')
+		print("PONG "+output.split(" ")[1])
 	output = output.strip('\n\r')
-	sender = output.split(":")[1].split("!")[0]
+	print(output)
+	try:
+		sender = output.split(":")[1].split("!")[0]
+	except:
+		print("")
 	try:
 		message = output.split(":")[2]
 	except:
 		print("")
-	if message == "PING":
-		irc.send("PONG :Pong\n")
-	elif message == "help":
+	if message == "help":
 		sendPrivateMessage(sender, "Commands:")
 		sendPrivateMessage(sender, "help - shows all commands")
 		sendPrivateMessage(sender, "game - shows the current game stats including the table")
